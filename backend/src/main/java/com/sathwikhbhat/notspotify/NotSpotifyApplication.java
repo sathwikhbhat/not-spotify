e54ebff87e89b2d9ae94fca2e10e19af98137099
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-05 18:52:37 +0530

    chore: initialize Spring Boot project with Maven setup and basic structure

---
+++ backend/src/main/java/com/sathwikhbhat/notspotify/NotSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.sathwikhbhat.notspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotSpotifyApplication.class, args);
+	}
+
+}

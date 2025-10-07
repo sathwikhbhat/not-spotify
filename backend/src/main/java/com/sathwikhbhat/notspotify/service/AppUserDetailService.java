a3a295f2d06788fe8d5b378ee846fb52b0747ba0
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-07 21:35:27 +0530

    feat: implement AppUserDetailService for user authentication

---
+++ backend/src/main/java/com/sathwikhbhat/notspotify/service/AppUserDetailService.java
@@ -0,0 +1,37 @@
+package com.sathwikhbhat.notspotify.service;
+
+import com.sathwikhbhat.notspotify.document.User;
+import com.sathwikhbhat.notspotify.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class AppUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                getAuthorities(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+}

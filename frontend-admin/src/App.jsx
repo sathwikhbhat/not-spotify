d759d4f68dfbf679d541219ab3fb63855b0aef62
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-18 10:13:28 +0530

    feat: enhance routing with ProtectedRoute for admin access control

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Login from './pages/Login'
 import AddSong from './pages/AddSong'
@@ -7,26 +6,26 @@ import ListSong from './pages/ListSong'
 import AddAlbum from './pages/AddAlbum'
 import ListAlbum from './pages/ListAlbum'
 import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
 
-export const API_BASE_URL = 'http://localhost:8080';
 
 const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Toaster position="top-center" reverseOrder={false} />
+        <Toaster position="top-center" />
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/add-song" element={<AddSong />} />
-          <Route path="/list-songs" element={<ListSong />} />
-          <Route path="/add-album" element={<AddAlbum />} />
-          <Route path="/list-albums" element={<ListAlbum />} />
+          <Route path="/add-song" element={<ProtectedRoute requireAdmin={true}><AddSong /></ProtectedRoute>} />
+          <Route path="/list-songs" element={<ProtectedRoute requireAdmin={true}><ListSong /></ProtectedRoute>} />
+          <Route path="/add-album" element={<ProtectedRoute requireAdmin={true}><AddAlbum /></ProtectedRoute>} />
+          <Route path="/list-albums" element={<ProtectedRoute requireAdmin={true}><ListAlbum /></ProtectedRoute>} />
           <Route path="/" element={<Login />} />
-          <Route path="*" element={<AddSong />} />
+          <Route path="*" element={<ProtectedRoute requireAdmin={true}><AddSong /></ProtectedRoute>} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
   )
 }
 
-export default App
+export default App;

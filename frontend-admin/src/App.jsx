c01154c02a599969bcb0e0f3469d3b2f1b3c7811
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-18 09:54:42 +0530

    feat: implement authentication context and provider for user management

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,6 +1,31 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Login from './pages/Login'
+import AddSong from './pages/AddSong'
+import ListSong from './pages/ListSong'
+import AddAlbum from './pages/AddAlbum'
+import ListAlbum from './pages/ListAlbum'
+import { AuthProvider } from './context/AuthContext'
+
+export const API_BASE_URL = 'http://localhost:8080';
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">App Component</div>
+    <AuthProvider>
+      <BrowserRouter>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/add-song" element={<AddSong />} />
+          <Route path="/list-songs" element={<ListSong />} />
+          <Route path="/add-album" element={<AddAlbum />} />
+          <Route path="/list-albums" element={<ListAlbum />} />
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<AddSong />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
